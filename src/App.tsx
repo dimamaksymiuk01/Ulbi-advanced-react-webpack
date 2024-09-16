a2a328682d0279164d4cacfe9195603b519ccb5d
dimamaksymiuk01 <admin@Dimas-MacBook-Air.local>
2024-09-16 17:02:14 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import Counter from "./components/Counter";
+import React, {Suspense} from 'react';
 import './index.scss';
+import {Link, Route, Routes} from 'react-router-dom';
+import {MainPage} from "./pages/MainPage/MainPage";
+import {AboutPage} from "./pages/AboutPage/AboutPage";
 
 const App = () => {
     return (
         <div className="app">
-            asdasdASFASFa
-            <Counter />
+            <Link to={'/'}>Main</Link>
+            <Link to={'/about'}>About us</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPage />} />
+                    <Route path={'/'} element={<MainPage />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
