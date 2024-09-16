71e9a1b953edee3bb198435370c99fcc927730b5
dimamaksymiuk01 <admin@Dimas-MacBook-Air.local>
2024-09-16 17:51:06 +0300

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React, {Suspense} from 'react';
-import './index.scss';
+import {Suspense} from 'react';
+import './styles/index.scss';
 import {Link, Route, Routes} from 'react-router-dom';
 import {MainPage} from "./pages/MainPage/MainPage";
 import {AboutPage} from "./pages/AboutPage/AboutPage";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
-            <Link to={'/'}>Main</Link>
-            <Link to={'/about'}>About us</Link>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
                     <Route path={'/about'} element={<AboutPage />} />
