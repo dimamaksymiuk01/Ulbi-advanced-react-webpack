d16b0c2c944c3e344a48bbf0f6d22d7a61759a21
dimamaksymiuk01 <admin@Dimas-MacBook-Air.local>
2024-09-17 17:31:26 +0300

    add sidebar and navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,23 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
+import {Navbar} from "widgets/Navbar";
 
 import {AppRouter} from "app/providers/router";
+import {Sidebar} from "widgets/Sidebar";
 
 
 const App = () => {
-   const { theme, toggleTheme } = useTheme();
+   const { theme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-            <AppRouter/>
+            <Navbar/>
+            <div className={'content-page'}>
+                <Sidebar/>
+                <AppRouter/>
+            </div>
+
         </div>
     );
 };
