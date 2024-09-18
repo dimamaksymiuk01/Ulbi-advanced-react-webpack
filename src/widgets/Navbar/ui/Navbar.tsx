16c8943641ab2912ad16d62c1ad9114919c267ad
dimamaksymiuk01 <admin@Dimas-MacBook-Air.local>
2024-09-18 15:52:46 +0300

    add eslint

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,29 +1,32 @@
-import {classNames} from "shared/lib/classNames/classNames";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
-import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
-import {useTranslation} from "react-i18next";
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = ({className}: NavbarProps) => {
-    const {t} = useTranslation();
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
 
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to="/"
+                    className={cls.mainLink}
+                >
                     {t('About us')}
                 </AppLink>
-                <AppLink theme={AppLinkTheme.RED} to={'/about'}>
+                <AppLink
+                    theme={AppLinkTheme.RED}
+                    to="/about"
+                >
                     {t('Main page')}
                 </AppLink>
             </div>
         </div>
     );
 };
-
-
-
-
